436782f9bb6e396ed3ccd35e61ed1e61d5f82ee2
Bleakness <windpourwow@gmail.com>
2024-03-01 20:13:48 +0100

    the silhouette generator is done and works properly in almost every case

--- picture_converter/silhouette_gen.py
+++ picture_converter/silhouette_gen.py
@@ -2,22 +2,20 @@ import os
 from PIL import Image
 
 
-def image_to_black(image_name, background_color=(0, 0, 0)):
-    img = Image.open(f"./images/{image_name}")
+def image_to_black(image_name):
+    im = Image.open(f"./images/{image_name}")
 
-    img = img.convert("RGBA")
+    image = im.convert("RGBA")
+    alpha = image.getchannel("A")
+    alphaThresh = alpha.point(lambda p: 255 if p > 200 else 0)
 
-    img_data = list(img.getdata())
+    # Make a new completely black image same size as original
+    res = Image.new("RGB", im.size)
 
-    for i, pixel in enumerate(img_data):
-
-        if pixel[:3] != background_color:
-            img_data[i] = (0, 0, 0, 255)
-
-    img.putdata(img_data)
-
-    img.save(f"./silhouette/{image_name}")
+    # Copy across the alpha channel from original
+    res.putalpha(alphaThresh)
+    res.save(f"./silhouette/{image_name}")
 
 
 for file in os.listdir("./images/"):
-    image_to_black(file)
+   image_to_black(file)
