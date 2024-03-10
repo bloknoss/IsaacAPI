9f0b9900e0bdcfeccc8b206fe2024e07a20e2bfb
bloknoss <windpourwow@gmail.com>
2024-03-10 19:17:47 +0100

    test

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const items = require("./data/items.json");
 const path = require("path");
+const fs = require("fs");
 
 const actives = [...items.items].filter((item) => item.type === "active");
 const passives = [...items.items].filter((item) => item.type === "passive");
@@ -9,25 +10,60 @@ const app = express();
 const PORT = 8080;
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 
+function readFilesSync(dir) {
+  const files = [];
+
+  fs.readdirSync(dir).forEach((filename) => {
+    const name = path.parse(filename).name;
+    const ext = path.parse(filename).ext;
+    const filepath = path.resolve(dir, filename);
+    const stat = fs.statSync(filepath);
+    const isFile = stat.isFile();
+
+    if (isFile) files.push({ filepath, name, ext });
+  });
+
+  files.sort((a, b) => {
+    // natural sort alphanumeric strings
+    // https://stackoverflow.com/a/38641281
+    return a.name.localeCompare(b.name, undefined, { numeric: true, sensitivity: "base" });
+  });
+
+  return files;
+}
+
+function renameKeys(obj, newKeys) {
+  const myObject = obj;
+
+  // Renaming the key from 'oldKey' to 'newKey'
+  const { oldKey: newKey, ...rest } = myObject;
+  const updatedObject = { newKey, ...rest };
+
+  console.log(updatedObject); // { newKey: 'value' }
+  return updatedObject;
+}
+
+app.get("/api/bosses/", (req, res) => {
+  const originalBosses = path.join(__dirname, "images/original");
+  const silhouetteBosses = path.join(__dirname, "images/original");
+
+  const bosses = readFilesSync(originalBosses);
+  const silhouette = readFilesSync(silhouetteBosses);
+
+  res.json({ original: bosses, silhouette: silhouette });
+});
+
 app.get("/api/boss/:name/original", (req, res) => {
   const bossName = req.params.name + ".png";
 
   const image = path.join(__dirname, "images/original", bossName);
-
-  if (require("fs").existsSync(image)) {
-    res.sendFile(image);
-  } else {
-    res.status(404).send("Image not found");
-  }
+  const json = { bossName: image };
 });
 
 app.get("/api/boss/:name/hidden", (req, res) => {
